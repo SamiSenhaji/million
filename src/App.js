21c32023a6725d5f13187969ceea62a198981fcb
Sami SENHAJI <Sami.SENHAJI@ainos.lu>
2021-10-29 17:34:45 +0200

    First page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="col-sm-12">
+     <div>
+ <h1 className="text-center text-warning">Time Entries</h1>
+ </div>
+ <form>
+    <div className="col-sm-6 mt-4">
+      <label for="exampleInputEmail1" className="form-label mt-4">Email address</label>
+      <div className="text-center col-sm-6 ">
+      <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+      </div>
+    </div>
+    <div className="col-sm-6 mt-4">
+      <label for="exampleInputPassword1" class="form-label mt-4">Password</label>
+      <div className="text-center col-sm-6">
+      <input type="password" className="form-control " id="exampleInputPassword1" placeholder="Password" />
+    </div>
+     </div>
+     <div className="col-sm-4 mt-4 text-center">
+      <button type="submit" className="btn btn-warning">Sign in</button>
+      </div>
+
+</form>
     </div>
   );
 }
---
+++ src/Component/Header/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+    render() {
+        return (
+            <div className="text-center">
+               <img src="LogoAinos.png" alt="LogoAinos" />;
+            </div>
+        )
+    }
+}
+
+export default Header
